186d4d6b52b6f2060223325d4c78fb1d08a10cbf
yashassay <yashassay@localhost>
2025-07-07 13:36:07 +0530

    commi

--- src/main/java/com/shanInfotech/flightJDBCApp/FlightJDBCApp/App.java
+++ src/main/java/com/shanInfotech/flightJDBCApp/FlightJDBCApp/App.java
@@ -3,7 +3,7 @@ package com.shanInfotech.flightJDBCApp.FlightJDBCApp;
 public class App {
     public static void main(String[] args) throws Exception {
         FlightDetailsDao fd=new FlightDetailsDao();
-       fd.addFlight(new FlightDetails());
+      // fd.addFlight(new FlightDetails());
         fd.getFlight();
     }
 }
